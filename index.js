12e4aa23380543595022baf67c33a897417b6b70
Breno Wruck Schneider <brenows@gmail.com>
2017-07-24 08:31:30 -0300

    Inde.js code review

--- index.js
+++ index.js
@@ -1,32 +1,34 @@
-var express = require('express');
-var path = require('path');
-var bodyParser = require('body-parser');
+//External require
+let express = require('express');
+let path = require('path');
+let bodyParser = require('body-parser');
+let cors = require('cors');
 
-var tasksController = require('./tasks/tasks.controller');
-var pingController = require('./ping/ping.controller');
+//Internal require
+let tasksController = require('./tasks/tasks.controller');
+let pingController = require('./ping/ping.controller');
 
-var app = express();
+let app = express();
 
 /* Allow access from anywhere, including from local app */
-var cors = require('cors');
 app.use(cors());
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
+//Url that points to the API Documentation
 app.use('/docs', express.static(path.join(__dirname, '/docs')))
 
 //Routes and Controllers configurations
+//Further routes starting from the routes below are defined in each controller.
 app.use('/tasks', tasksController);
 app.use('/ping', pingController);
 
-
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-	"use strict";
-	var err = new Error('Not Found');
+	let err = new Error('Not Found');
 	err.status = 404;
-	next(err);
+	res.status(404).send(err.message);
 });
 
 //Default port is 4001 if not set by environment variable
@@ -34,6 +36,5 @@ app.set('port', process.env.PORT || '4001');
 
 //Start the server
 var server = app.listen(app.get('port'), function () {
-	"use strict";
 	console.log ('Lets Do It API listening on port ' + server.address().port);
 });
