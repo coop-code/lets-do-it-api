012dfc9c4576c6d924b1cac756e089e191c97d3c
Breno Wruck Schneider <brenows@gmail.com>
2017-08-02 14:27:27 -0300

    Config file name changed.

---
+++ config/main.js
@@ -0,0 +1,4 @@
+module.exports = {
+    'connectionString' : 'mongodb://lets-do-it-api:lets-do-it-password@clustermongodb-shard-00-00-fpkbe.mongodb.net:27017,clustermongodb-shard-00-01-fpkbe.mongodb.net:27017,clustermongodb-shard-00-02-fpkbe.mongodb.net:27017/letsdoit?ssl=true&replicaSet=ClusterMongoDB-shard-0&authSource=admin',
+    'port' : process.env.port || 4001
+}
